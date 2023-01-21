6c1707e76226c3d2c4f1e34b64ce611c9bd2d911
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-01-21 23:07:03 +0530

    Update DSAday1.cpp

--- DSAday1.cpp
+++ DSAday1.cpp
@@ -1,5 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*void hello(){
+    cout<<"Helooo!";
+}
+void Name(string name){
+    cout<<"hey "<<name<<endl;
+}
+int sum(int a, int b){
+   int  result=a+b;
+    return result;
+}
+//pass by value where actual value is not change its copy value will change!
+void notdoneyet(int n){
+    cout<<n<<endl;
+    n*=5;
+    cout<<n<<endl;
+    n*=7;
+    cout<<n<<endl;
+}
+
+//pass by reference where actual value is change its when it's copy value will change!
+void notdoneyet(int &n){
+    cout<<n<<endl;
+    n*=5;
+    cout<<n<<endl;
+    n*=7;
+    cout<<n<<endl;}
+*/
 int main(){
    /* cout<<"Welcome to the DSA first lecture!";
     long long x;
@@ -24,6 +51,7 @@ if(age>=18){
     else{
         cout<<"U R not adult";
     }*/
+/*
 //wap for the grading system with the help of if else statements
 int x;
 cout<<"Enter the marks:";
@@ -41,11 +69,114 @@ cout<<"grade B";
 else if (x>=80 & x<=100)
 cout<<"grade A+"; 
 else
-cout<<"enter the valid marks:";
+cout<<"enter the valid marks:";*/
+/*
+//WAP with the help of age restriction that a person is eligible for the job or not!
+int age;
+cout<<"Enter the age:";
+cin>>age;
+if (age<18){
+cout<<"not eligible for the job:";}
+ else if (age<=57){
+        cout<<"eligible for the job:";
+ if (age>=55){
+        cout<<",but retirement comming soon!";
+    }
+ }
+else
+cout<<"retirement";
+*/
 
+//WAP with the help of SwItcH StAtEmEnT
 
+/*int day;
+cout<<"Enter the day:";
+cin>>day;
+switch(day){
+    case 1:
+    cout<<"Monday";
+    break;
+    case 2:
+    cout<<"Tuesday";
+    break;
+    case 3:
+    cout<<"Wednesday";
+    break;
+    case 4:
+    cout<<"Thursday";
+    break;
+    case 5:
+    cout<<"Friday";
+    break;
+    case 6:
+    cout<<"Saturday";
+    break;
+    case 7:
+    cout<<"Sunday";
+    break;
+default:
+cout<<"invalid input:";
+}*/
 
+/*int choice;
+cout<<"Enter the choice:";
+cin>>choice;
+switch(choice){
+    case 1:
+    int radius;
+    cout<<"Enter the radius:";
+    cin>>radius;
+    cout<<"Area of Circle:"<<3.14*radius*radius;
+    break;
+    case 2:
+    int length,breadth;
+    cout<<"Enter length & breadth:";
+    cin>>length>>breadth;
+    cout<<"Area of Rectangle:"<<length*breadth;
+    break;
+}*/
 
+/*
+string s;
+cin>>s;
+cout<<s[0]<<s[1];
+int len=s.size();
+cout<<len;
+cout<<s[len-1];*/
 
-    return 0;
+/*hello();
+string name;
+cin>>name;
+Name(name);
+string name2;
+cin>>name2;
+Name(name2);
+
+//wap take two numbers and print their sum with the help of the functions
+int a,b;
+cin>>a>>b;
+cout<<sum(a,b);
+   
+//pass by value where actual value is not change its copy value will change!
+ int n;
+ cin>>n;
+ notdoneyet(n);
+ cout<<n; 
+
+ //pass by reference actual value will change if there is change in the function!
+ int n;
+ cin>>n;
+ notdoneyet(n);
+ cout<<n;*/
+ int n;
+ cin>>n;
+ int arr[n];
+ for (int i=0;i<n;i++){
+    cin>>arr[i];
+ }
+ for (int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+ }
+ cout<<endl;
+return 0;
 }
