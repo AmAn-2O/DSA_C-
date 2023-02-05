904120e650d7d781e4da2a7e74207af26d4e634f
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-02-06 01:40:57 +0530

    Update DSAday4_Basics_Maths.cpp

--- DSAday4_Basics_Maths.cpp
+++ DSAday4_Basics_Maths.cpp
@@ -59,7 +59,104 @@ while(m>0){
 if(reverse==n) 
 {cout<<"Palindrome";}
 else
-{ cout<<"!Palindrome";}*/
+{ cout<<"!Palindrome";}
+
+//check whether a no. is Armstrong or !
+// 371 =3^3+7^3+1^3=371 than the no. is armstrong no.
+int m=n;
+int sum=0;
+while(m>0){
+    int ld=m%10;
+    m/=10;
+    sum=sum+(ld*ld*ld);
+}
+if (n==sum) cout<<"Armstrong No.";
+else cout<<"!Armstrong No.";
+
+//find all the divisors 
+// 12 divisors are 1,2,3,4,6,12
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+        cout<<i<<" ";
+    }
+}
+
+vector<int> ls;
+for(int i=1;i<=sqrt(n);i++){
+    if(n%i==0){
+        ls.push_back(i);
+    
+    if((n/i)!=i){
+        ls.push_back(n/i);}
+    
+    }
+}
+sort(ls.begin(),ls.end());
+for(auto it:ls){
+    cout<<it<<" ";
+}
+
+//check whether a no. is prime or not
+int count=0;
+for(int i=2;i<n;i++){
+    if(n%i==0){
+        count++;
+    }
+}
+if(count==0) cout<<"Prime";
+else cout<<"!Prime";
+
+//print the all the prime no. b\w the given range
+for(int i=1;i<=n;i++){
+    for(int j=2;j<i;j++){
+        if(i%j==0){
+            count++;
+        }
+    }
+    if(count==0&& i!=0) {
+    cout<<i<<" ";}
+    count=0;
+}
+
+//find GCD/HCf of a no.
+int m,m1;
+cout<<"Enter the no.";
+cin>>m>>m1;
+int gcd=1;
+for(int i=1;i<=min(m,m1);i++){
+    if(m%i==0&& m1%i==0){
+        gcd=i;
+    }
+}
+cout<<gcd;
+
+//more efficient way to find gcd/hcf of a no. by inversing the loop
+int m,m1;
+cout<<"Enter the no.";
+cin>>m>>m1;
+int gcd=1;
+for(int i=min(m,m1);i>1;i--){
+    if(m%i==0&& m1%i==0){
+        gcd=i;
+    }
+}
+cout<<gcd;
+
+//best way to find the gcd is Euclidean Algorithm 
+//gcd(a,b)==gcd(a-b,b) whether a>b
+int a,b;
+cout<<"Enter the no.";
+cin>>a>>b;
+while(a>0 && b>0){
+    if(a>b){
+        a=a%b;}
+        else{
+        b=b%a;}
+}
+if(a==0)
+{ cout<<b;}
+else
+{ cout<<a;}*/
 
 return 0;
     }
