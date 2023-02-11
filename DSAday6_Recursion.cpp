612304df0b8363b6dd371ba4a9c73cb6c712b2a9
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-02-12 01:56:49 +0530

    Update DSAday6_Recursion.cpp

--- DSAday6_Recursion.cpp
+++ DSAday6_Recursion.cpp
@@ -1,18 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-void sum_recursion(int i, int sum){
+/*void fact_recursion_pr(int i,int fact){
+    if(i<1){
+        cout<<fact;
+        return ;
+    }
+    fact_recursion_pr(i-1,fact*i);
+}
+int fact_recursion_fun(int n){
+    if(n==0) return 1;
+    return n*fact_recursion_fun(n-1);
+}
+void sum_recursion_pr(int i, int sum){
     if(i<1){
         cout<<sum;
         return ;
     }
-    sum_recursion(i-1,sum+i);
+    sum_recursion_pr(i-1,sum+i);
 }
+int sum_recursion_fun(int n){
+    if(n==0){
+        return 0;
+    }
+    return n+sum_recursion_fun(n-1);
+}
+int recursion_fib(int n){
+    if(n<1)return 1;
+    return recursion_fib(n-1)+recursion_fib(n-2);
+}*/
 int main(){
 //print the sum of first N natural no.s with the help of recursion
 int n;
 cout<<"Enter the no.";
 cin>>n;
-sum_recursion(n,0);
+// sum_recursion_pr(n,0);
+// cout<<endl;
+// cout<<sum_recursion_fun(n)<<endl;
+// fact_recursion_pr(n,1);
+// cout<<endl;
+// cout<<fact_recursion_fun(n);
+
     return 0;
 
 }
