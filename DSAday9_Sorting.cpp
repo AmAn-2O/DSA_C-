7e54d0872b2463d8c5bd6012b64857c9838cf87c
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-02-21 01:19:50 +0530

    Update DSAday9_Sorting.cpp

--- DSAday9_Sorting.cpp
+++ DSAday9_Sorting.cpp
@@ -1,5 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+void insertion_sort(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    int j = i;
+    // while(j>0&&arr[j-1]>arr[j])
+    while (arr[j] > arr[j + 1]) {
+      int temp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = temp;
+      j--;
+    }
+  }
+}
+void bubble_sort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j <= n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+void bubble_sort_optimizedver(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int swap = 0;
+    for (int j = 0; j <= n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        swap = 1;
+      }
+    }
+    if (swap == 0) {
+      break;
+    }
+  }
+}
 void selection_sort(int arr[], int n) {
   for (int i = 0; i < n - 2; i++) {
     int min = i;
@@ -39,7 +78,12 @@ int main() {
     cin >> arr[i];
   }
   selection_sort(arr, n);
+  bubble_sort(arr, n);
+  bubble_sort_optimizedver(arr, n);
   cout << "Sorting According to Selection Sort:";
+  cout << "Sorting According to Bubble Sort:";
+  insertion_sort(arr, n);
+  cout << "Sorting According to Insertion Sort:";
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
