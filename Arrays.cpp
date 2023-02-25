00e3867cee94603b194c3a3ff743f6a0e8285762
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-02-25 15:54:06 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,17 +1,81 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    //first way to wap to intialises with array with any no.
-    int n;
-    cout<<"Enter the value:";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        arr[i]=1;
-    }
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
-    }
+void print_array(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+int main() {
+  //WAP TO INTIALIZE THE ARRAY WITH ANY NO.
+//   int n;
+//   cout << "Enter the value:";
+//   cin >> n;
+//   int arr[n];
+  // int arr[5];
 
-    return 0;
+  //WAP TO FIND THE MINIMUM AND MAXIMUM ELEMENT OF AN ARRAY
+
+//   int maximum = INT_MIN;
+//   int minimum = INT_MAX;
+
+//   for (int i = 0; i < n; i++) {
+//     cin >> arr[i];
+//   }
+//   for (int i = 0; i < n; i++) {
+//     maximum = max(maximum, arr[i]);
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     minimum = min(minimum, arr[i]);
+//     else if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   cout << "smaller" << minimum << "bigger" << maximum;
+
+//WAP TO FIND THE SUM OF ALL THE ELEMENTS OF ARRAY
+// for (int i = 0; i < n; i++) {
+//     cin >> arr[i];
+//   }
+// int sum=0;
+// for (int i = 0; i < n; i++) {
+//     sum+=arr[i];
+//   }
+// cout<<sum;
+
+
+//WAP TO IMPLEMENT THE LINEAR SEARCH
+
+// for (int i = 0; i <sizeof(arr[i]); i++) {
+//     cin >> arr[i];
+//   }
+//   cout<<"Enter the key";
+// int m;
+// cin>>m;
+// for(int i=0;i<sizeof(arr[i]);i++){
+//     if (arr[i]==m){
+//         cout<<"Element founded at index no."<<i;
+//     }
+// }
+
+//WAP TO REVERSE AN ARRAY
+// int n;
+// cin>>n;
+// int arr[n];
+// for(int i=0;i<n;i++){
+//   cin>>arr[i];
+// }
+// int start=0;
+// int end=n-1;
+// while(start<=end){
+// int temp=arr[start];
+// arr[start]=arr[end];
+// arr[end]=temp;
+// start++;
+// end--;
+// }
+// for(int i=0;i<n;i++){
+//   cout<<arr[i]<<" ";
+// }
+  return 0;
 }
