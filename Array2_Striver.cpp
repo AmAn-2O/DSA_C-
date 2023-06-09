b83d2a0af0f12955b96be6e356726f0a8de351ae
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-06-09 23:58:00 +0530

    Update Array2_Striver.cpp

--- Array2_Striver.cpp
+++ Array2_Striver.cpp
@@ -7,12 +7,12 @@ int linear_Search(int arr[],int  n,int number){
     return -1;
 }
 vector<int> find_Un(int arr1[], int arr2[], int n1,int n2){
-    set<int> st;
+    set<int> st1;
     for(int i=0;i<n1;vi++){
-        st.insert(arr1[i]);
+        st1.insert(arr1[i]);
     }
     for(int i=0;i<n2;i++){
-      st.insert(arr2[i]);
+      st1.insert(arr2[i]);
     }
     vector<int> temp;
     for(auto it:st){
